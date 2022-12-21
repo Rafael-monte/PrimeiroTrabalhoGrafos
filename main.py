e78c4c2da85653398179dac3d55c72b718b30c15
RafaelMonte <rafaelmonte46@gmail.com>
2022-12-21 14:42:12 -0300

    feature/mostrar-vertices-adjacentes #Adições:  - Metodo para buscar vertices adjacentes

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from classes.grafo import Grafo
 from classes.leitor_arquivo import LeitorArquivo
+from classes.vertice import Vertice
 
 if __name__ == '__main__':
     leitor: LeitorArquivo = LeitorArquivo()
     grafo: Grafo = leitor.ler_arquivo_entrada()
     grafo.criar_matriz_adjacencias()
-
+    vertices: list[Vertice] = grafo.buscar_vertices_adjacentes('b', printar=True)
