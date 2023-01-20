8229e3c78586405feb34a07333c21abaa2fd09ec
Rafael-monteiro <rafaelmonte46@gmail.com>
2023-01-19 22:01:33 -0300

    Criando grafo transposto

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from classes.leitor_arquivo import LeitorArquivo
 if __name__ == '__main__':
     leitor: LeitorArquivo = LeitorArquivo()
     grafo: Grafo = leitor.ler_arquivo_entrada()
+    grafo.criar_matriz_adjacencias()
+    print('----------------------------------------------------')
     grafo_complemento: Grafo = CriadorGrafoComplemento.criar_grafo_complemento_a_partir_de_grafo(grafo)
     #grafo_complemento.mostrar_grafo()
     grafo_complemento.criar_matriz_adjacencias()
